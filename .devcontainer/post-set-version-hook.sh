9efaa70b42214bac2509f39692d9762d961749c4
Elahd Bar-Shai <466460+elahd@users.noreply.github.com>
2022-12-16 15:06:58 -0500

    Update post-set-version-hook.sh

--- .devcontainer/post-set-version-hook.sh
+++ .devcontainer/post-set-version-hook.sh
@@ -10,12 +10,12 @@ fi
 
 repo_url="https://github.com/home-assistant/core.git"
 git_root="/workspaces/core"
-workspace_dir=${WORKSPACE_DIRECTORY}
+workspace_dir="${WORKSPACE_DIRECTORY}"
 
 # Delete integration pylint directory if no version entered.
 if [ -z "$1" ]; then
     echo "No version supplied. Deleting existing development files."
-    rm -rf $dev_dir/pylint
+    rm -rf "$workspace_dir/pylint"
     exit 1
 fi
 
@@ -31,14 +31,14 @@ else
 fi
 
 # Move pylint fiiles to integration pylint directory
-mkdir -p "$dev_dir/pylint/plugins"
+mkdir -p "$workspace_dir/pylint/plugins"
 
-if [ "$(readlink $dev_dir/pylint)" != "$git_root/pylint" ]; then
-    rm -rf "$dev_dir/pylint/plugins"
-    cp -r "$git_root/pylint/plugins" "$dev_dir/pylint/plugins"
+if [ "$(readlink "$workspace_dir"/pylint\")" != "$git_root/pylint" ]; then
+    rm -rf "$workspace_dir/pylint/plugins"
+    cp -r "$git_root/pylint/plugins" "$workspace_dir/pylint/plugins"
 fi
 
 # Symlink integration's custom_components folder into checked out code to enable hassfest.
-if [ "$(readlink $git_root/homeassistant/components/$integration_name)" != "$dev_dir/custom_components/$integration_name" ]; then
-    ln -s $dev_dir/custom_components/$integration_name $git_root/homeassistant/components/$integration_name
+if [ "$(readlink "$git_root"/homeassistant/components/"$integration_name")" != "$workspace_dir"/custom_components/"$integration_name" ]; then
+    ln -s "$workspace_dir""/custom_components/""$integration_name" "$git_root""/homeassistant/components/""$integration_name"
 fi
