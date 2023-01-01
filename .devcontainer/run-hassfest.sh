f85c477152880511bc64b33e6b3a897586b760f9
Elahd Bar-Shai <466460+elahd@users.noreply.github.com>
2023-01-01 04:27:04 +0000

    Codacy fix

--- .devcontainer/run-hassfest.sh
+++ .devcontainer/run-hassfest.sh
@@ -6,4 +6,4 @@ if [[ -z "${INTEGRATION_NAME}" ]] || [[ -z "${WORKSPACE_DIRECTORY}" ]]; then
   exit 1
 fi
 
-/workspaces/core/script/run-in-env.sh python3 -m script.hassfest --action validate --integration-path ${WORKSPACE_DIRECTORY}/custom_components/${INTEGRATION_NAME} -p application_credentials,bluetooth,codeowners,dependencies,dhcp,json,manifest,mqtt,services,ssdp,translations,usb,zeroconf,config_flow,coverage,mypy_config,metadata
+/workspaces/core/script/run-in-env.sh python3 -m script.hassfest --action validate --integration-path "${WORKSPACE_DIRECTORY}/custom_components/${INTEGRATION_NAME}" -p application_credentials,bluetooth,codeowners,dependencies,dhcp,json,manifest,mqtt,services,ssdp,translations,usb,zeroconf,config_flow,coverage,mypy_config,metadata
