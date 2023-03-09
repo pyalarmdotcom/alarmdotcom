0c9e0e4a1f3d6d40ddece1ba578961345690b4fd
Elahd Bar-Shai <466460+elahd@users.noreply.github.com>
2023-03-08 23:52:02 -0500

    Devcontainer updates (#231)
    * Merge branch 'websocket' - merging in changes to linting and devcontainer.
    
    * Tweaks
    
    * Fix python path issues
    
    * autofix
    
    * Simplify pre-commit / linting scripts.
    
    * Removed yamllint. Covered by prettier.
    
    * File permissions fix
    
    * Gitignore to ignore Home Assistant files.
    
    * More gitignore
    
    * Bump version

---
+++ scripts/setup.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+if [ -z "${LIBRARY_NAME}" ] || [ -z "${LIBRARY_GIT_URL}" ] || [ -z "${WORKSPACE_DIRECTORY}" ]; then
+  exit 1
+else
+  library_name="${LIBRARY_NAME}"
+  repo_url="${LIBRARY_GIT_URL}"
+  lib_dir="/workspaces/$library_name"
+  workspace_dir="${WORKSPACE_DIRECTORY}"
+fi
+
+cat << EOF
+
+###########################
+INSTALLING DEV REQUIREMENTS
+###########################
+
+EOF
+
+pip install --upgrade pip
+pip install -r "$workspace_dir/requirements-dev.txt"
+
+cat << EOF
+
+####################################
+INITIALIZING LIBRARY "$library_name"
+####################################
+
+EOF
+
+if [ ! -d "$lib_dir" ]; then
+    echo "Cloning $library_name repository..."
+    git clone "$repo_url" "$lib_dir"
+else
+    echo "$library_name repository directory already exists."
+fi
+
+(cd "$lib_dir"; pip install --editable .)
+
+pip install -r "$lib_dir/requirements-dev.txt"
+
+cat << EOF
+
+#####################
+INSTALLING PRE-COMMIT
+#####################
+
+EOF
+
+# For integration
+pre-commit install
+pre-commit install-hooks
+
+# For library
+(cd "$lib_dir"; pre-commit install)
+(cd "$lib_dir"; pre-commit install-hooks)
+
+cat << EOF
+
+#####
+DONE!
+#####
+
+EOF
+
+exit 0
